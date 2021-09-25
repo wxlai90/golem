6f520e48ea982e2dcaa66daa7b953f8807916f44
lwx <l@w.x>
2021-09-25 15:30:09 +0800

    first cut

--- main.go
+++ main.go
@@ -1,2 +1,21 @@
-package golem
+package main
 
+import (
+	"golem/router"
+	"io"
+	"net/http"
+)
+
+func main(){
+	r := router.New()
+	
+	r.GET("/", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "GET: Hello World")
+	})
+
+	r.POST("/", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "POST: Hello Again World!")
+	})
+
+	http.ListenAndServe(":5000", r)
+}

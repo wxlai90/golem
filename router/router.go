1880286dd3c93b447dda1d033296102f77b923e6
lwx <l@w.x>
2021-12-30 16:25:58 +0800

    remove driver

--- router/router.go
+++ router/router.go
@@ -4,15 +4,13 @@ import (
 	"net/http"
 )
 
-
 var handlers map[string]map[string]Handler = map[string]map[string]Handler{}
 
-type Router struct {}
+type Router struct{}
 
 type Handler func(http.ResponseWriter, *http.Request)
 
-
-func (ro *Router) ServeHTTP(w http.ResponseWriter, r *http.Request){
+func (ro *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if handler, ok := handlers[r.Method][r.URL.Path]; ok {
 		handler(w, r)
 	} else {
@@ -20,8 +18,7 @@ func (ro *Router) ServeHTTP(w http.ResponseWriter, r *http.Request){
 	}
 }
 
-
-func addPathToHandlers(method string, path string, handler Handler){
+func addPathToHandlers(method string, path string, handler Handler) {
 	_, exists := handlers[method]
 
 	if !exists {
@@ -30,12 +27,24 @@ func addPathToHandlers(method string, path string, handler Handler){
 	}
 }
 
-func (r *Router) GET(path string, handler Handler){
+func (r *Router) GET(path string, handler Handler) {
 	addPathToHandlers(http.MethodGet, path, handler)
 }
 
-func (r *Router) POST(path string, handler Handler){
+func (r *Router) POST(path string, handler Handler) {
 	addPathToHandlers(http.MethodPost, path, handler)
+}
+
+func (r *Router) PUT(path string, handler Handler) {
+	addPathToHandlers(http.MethodPut, path, handler)
+}
+
+func (r *Router) PATCH(path string, handler Handler) {
+	addPathToHandlers(http.MethodPatch, path, handler)
+}
+
+func (r *Router) DELETE(path string, handler Handler) {
+	addPathToHandlers(http.MethodDelete, path, handler)
 }
 
 func New() *Router {
